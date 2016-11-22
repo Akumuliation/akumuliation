700bee37df5c3f0d85d135cacd1c891e3b755409
Akumuliation <vetalia777@mail.ru>
2016-11-22 09:47:18 +0200

    1.2

--- novosti.php
+++ novosti.php
@@ -1,3 +1,4 @@
+echo "
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript">
  $(document).ready(function() {
@@ -7,3 +8,4 @@
     $("a").attr("target","_blanck");
     });
 </script>
+";
