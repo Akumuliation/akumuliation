b0a3587de767232b38a1ddbe5400f8155842659f
Vitalii <akumuliation@gmail.com>
2018-07-12 16:01:21 +0300

    Delete novosti-n.js

--- novostnik/novosti-n.js
+++
@@ -1,2 +0,0 @@
-document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script><script type="text/javascript">$(document).ready(function() {$("a").click(function () {document.location.replace( novost );});$("a").attr("target","_blanck");});</script>
-');
